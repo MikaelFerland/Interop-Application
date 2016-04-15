0d50a15cf140cb40bfc9b41787f54fdf1e25368b
DannyL <DannyL@CEL01INFO05009>
2016-04-15 15:26:23 -0400

    Implement IInstanceProvider to be able to call a parametrized service contructor.

--- Desktop/Interop.Modules.Client/Services/TargetService.cs
+++ Desktop/Interop.Modules.Client/Services/TargetService.cs
@@ -10,7 +10,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Description;
+using System.ServiceModel.Channels;
 
 namespace Interop.Modules.Client.Services
 {
@@ -37,7 +39,9 @@ namespace Interop.Modules.Client.Services
 
             Uri baseAddress = new Uri($"net.tcp://{myIP}:8000/targetserver");
             _serviceHost = new ServiceHost(typeof(Server.TargetServer), baseAddress);
+            var instanceProvider = new InstanceProviderBehavior<ITargetServer>(() => new Server.TargetServer(eventAggregator));
             _serviceHost.AddServiceEndpoint(typeof(ITargetServer), binding, baseAddress);
+            instanceProvider.AddToAllContracts(_serviceHost);
 
             _serviceHost.Open();
                 
@@ -62,5 +66,74 @@ namespace Interop.Modules.Client.Services
             }
             throw new Exception("Local IP Address Not Found!");
         }
+    }
+    public class InstanceProviderBehavior<T> : IInstanceProvider, IContractBehavior
+        where T : class
+    {
+        private readonly Func<T> _instanceProvider;
+
+        public InstanceProviderBehavior(Func<T> instanceProvider)
+        {
+            _instanceProvider = instanceProvider;
+        }
+
+        // I think this method is more suitable to be an extension method of ServiceHost.
+        // I put it here in order to simplify the code.
+        public void AddToAllContracts(ServiceHost serviceHost)
+        {
+            foreach (var endpoint in serviceHost.Description.Endpoints)
+            {
+                endpoint.Contract.Behaviors.Add(this);
+            }
+        }
+
+        #region IInstanceProvider Members
+
+        public object GetInstance(InstanceContext instanceContext, Message message)
+        {
+            return this.GetInstance(instanceContext);
+        }
+
+        public object GetInstance(InstanceContext instanceContext)
+        {
+            // Create a new instance of T
+            return _instanceProvider.Invoke();
+        }
+
+        public void ReleaseInstance(InstanceContext instanceContext, object instance)
+        {
+            try
+            {
+                var disposable = instance as IDisposable;
+                if (disposable != null)
+                {
+                    disposable.Dispose();
+                }
+            }
+            catch { }
+        }
+
+        #endregion
+
+        #region IContractBehavior Members
+
+        public void AddBindingParameters(ContractDescription contractDescription, ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
+        {
+        }
+
+        public void ApplyClientBehavior(ContractDescription contractDescription, ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+        }
+
+        public void ApplyDispatchBehavior(ContractDescription contractDescription, ServiceEndpoint endpoint, DispatchRuntime dispatchRuntime)
+        {
+            dispatchRuntime.InstanceProvider = this;
+        }
+
+        public void Validate(ContractDescription contractDescription, ServiceEndpoint endpoint)
+        {
+        }
+
+        #endregion
     }
 }
