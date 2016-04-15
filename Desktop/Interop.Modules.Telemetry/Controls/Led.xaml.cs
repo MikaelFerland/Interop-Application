0652510329b06e32cfe3620c511d5ab9759a58c8
Mikael Ferland <mikael.ferland.1@etsmtl.net>
2016-04-14 20:50:37 -0400

    Telemetry region is populated

---
+++ Desktop/Interop.Modules.Telemetry/Controls/Led.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Interop.Modules.Telemetry.Controls
+{
+    /// <summary>
+    /// Interaction logic for Led.xaml
+    /// </summary>
+    public partial class Led : UserControl
+    {
+        public Led()
+        {
+            InitializeComponent();
+            if (this.IsActive)
+                this.backgroundColor.Color = ColorOn;
+            else
+                this.backgroundColor.Color = ColorOff;
+        }
+        
+        // Credit to http://mestaa.blogspot.ca/2011/04/led-for-wpf-how-to-create-wpf-user.html
+        private static void IsActivePropertyChanced(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Led led = (Led)d;
+            if ((bool)e.NewValue)
+                led.backgroundColor.Color = led.ColorOn;
+            else
+                led.backgroundColor.Color = led.ColorOff;
+        }
+
+        private static void OnColorOnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Led led = (Led)d;
+            led.ColorOn = (Color)e.NewValue;
+            if (led.IsActive)
+                led.backgroundColor.Color = led.ColorOn;
+            else
+                led.backgroundColor.Color = led.ColorOff;
+        }
+
+        private static void OnColorOffPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Led led = (Led)d;
+            led.ColorOff = (Color)e.NewValue;
+            if (led.IsActive)
+                led.backgroundColor.Color = led.ColorOn;
+            else
+                led.backgroundColor.Color = led.ColorOff;
+        }
+
+        /// <summary>Dependency property to Get/Set the current IsActive (True/False)</summary>
+        public static readonly DependencyProperty IsActiveProperty =
+            DependencyProperty.Register("IsActive", typeof(bool), typeof(Led),// null);
+                new PropertyMetadata(new PropertyChangedCallback(Led.IsActivePropertyChanced)));
+
+        /// <summary>Dependency property to Get/Set Color when IsActive is true</summary>
+        public static readonly DependencyProperty ColorOnProperty =
+            DependencyProperty.Register("ColorOn", typeof(Color), typeof(Led), //null);
+                new PropertyMetadata(Colors.Green, new PropertyChangedCallback(Led.OnColorOnPropertyChanged)));
+
+        /// <summary>Dependency property to Get/Set Color when IsActive is false</summary>
+        public static readonly DependencyProperty ColorOffProperty =
+            DependencyProperty.Register("ColorOff", typeof(Color), typeof(Led),// null);
+                new PropertyMetadata(Colors.Red, new PropertyChangedCallback(Led.OnColorOffPropertyChanged)));
+
+        /// <summary>Gets/Sets Value</summary>
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set
+            {
+                SetValue(IsActiveProperty, value);
+            }
+        }
+
+        /// <summary>Gets/Sets Color when led is True</summary>
+        public Color ColorOn
+        {
+            get
+            {
+                return (Color)GetValue(ColorOnProperty);
+            }
+            set
+            {
+                SetValue(ColorOnProperty, value);
+            }
+        }
+
+        /// <summary>Gets/Sets Color when led is False</summary>
+        public Color ColorOff
+        {
+            get
+            {
+                return (Color)GetValue(ColorOffProperty);
+            }
+            set
+            {
+                SetValue(ColorOffProperty, value);
+            }
+        }
+    }
+}
--- Desktop/Interop.Modules.Telemetry/ViewModels/TelemetryViewModel.cs
+++ Desktop/Interop.Modules.Telemetry/ViewModels/TelemetryViewModel.cs
@@ -13,32 +13,71 @@ namespace Interop.Modules.Telemetry.ViewModels
 {
 	public class TelemetryViewModel : BindableBase
 	{
-        IEventAggregator _eventAggregator;
+		IEventAggregator _eventAggregator;
 
-        public TelemetryViewModel(IEventAggregator eventAggregator, ITelemetryService telemetryService)
-        {
-            if (eventAggregator == null)
-            {
-                throw new ArgumentNullException("eventAggregator");
-            }
+		public TelemetryViewModel(IEventAggregator eventAggregator, ITelemetryService telemetryService)
+		{
+			if (eventAggregator == null)
+			{
+				throw new ArgumentNullException("eventAggregator");
+			}
 
-            if (telemetryService == null)
-            {
-                throw new ArgumentNullException("telemetryService");
-            }
+			if (telemetryService == null)
+			{
+				throw new ArgumentNullException("telemetryService");
+			}
 
-            _eventAggregator = eventAggregator;
+			_eventAggregator = eventAggregator;
 
-            _eventAggregator.GetEvent<UpdateTelemetry>().Subscribe(Update_Telemetry);
+			_eventAggregator.GetEvent<UpdateTelemetry>().Subscribe(Update_Telemetry);
 
-            Title = "Telemetry Region";
+			Title = "Telemetry Region";
+		}
+
+		public string Title { get; set; }
+
+		public void Update_Telemetry(Infrastructure.Models.DroneTelemetry droneTelemetry)
+		{            
+			DronePosition = $"{droneTelemetry.Latitutde.ToString()}, {droneTelemetry.Longitude.ToString()}";
+            DroneHeading = $"{droneTelemetry.Heading.ToString()}";
         }
 
-        public string Title { get; set; }
+		/// <summary>
+		/// 
+		/// </summary>
+		string _dronePosition = string.Empty;
+		public string DronePosition
+		{
+			get
+			{
+				return _dronePosition;
+			}
+			set
+			{
+				if (SetProperty(ref _dronePosition, value))
+				{
+					//this.OnPropertyChanged(() => this.);
+				}
+			}
+		}
 
-        public void Update_Telemetry(Infrastructure.Models.DroneTelemetry droneTelemetry)
+        /// <summary>
+        /// 
+        /// </summary>
+        string _droneHeading = string.Empty;
+        public string DroneHeading
         {
-
+            get
+            {
+                return _droneHeading;
+            }
+            set
+            {
+                if (SetProperty(ref _droneHeading, value))
+                {
+                    //this.OnPropertyChanged(() => this.);
+                }
+            }
         }
     }
 }
